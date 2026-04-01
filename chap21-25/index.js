82da30385e5cca8d3f10b2aa36f9a202b1b5d18c
hibafayyan <hibafayyan@gmail.com>
2026-04-02 02:56:09 +0500

    add assignment

---
+++ chap21-25/index.js
@@ -0,0 +1,119 @@
+// 1. Write a program that takes two user inputs for first and 
+// last name using prompt and merge them in a new variable 
+// titled fullName. Greet the user using his full name.
+
+// let firstName=prompt("Enter your first name ");
+// let lastName=prompt("Enter your last name ");
+
+
+// document.writeln(firstName + lastName);
+
+// _______________________________________________________________
+
+
+// 2. Write a program to take a user input about his favorite 
+// mobile phone model. Find and display the length of user 
+// input in your browser 
+
+// let info=prompt("Enter your favorite mobile phone model");
+// document.writeln(`My Favourite phone is ${info} <br>`);
+// document.writeln(`lenght of string is :  ${info.length}`)
+
+// _________________________________________________________________________
+
+// 3. Write a program to find the index of letter “n” in the word 
+// “Pakistani” and display the result in your browser . 
+
+// let word= "Pakistani";
+// let index= word.indexOf("n");
+
+// document.writeln("String: " + word + "<br>");
+// document.writeln("Index of 'n': " + index);
+
+
+// 4. Write a program to find the last index of letter “l” in the 
+// word “Hello World” and display the result in your browser.
+
+
+// let msg="Hello World";
+// let index=msg.lastIndexOf("l");
+
+// document.writeln(`
+//     Srting:${msg}<br>
+//     last index of 'l' is ${index}
+//      `)
+// _________________________________________________________________________
+
+// 5. Write a program to find the character at 3rd index in the 
+// word “Pakistani” and display the result in your browser. 
+
+// let word="Pakistani";
+// let char=word.charAt(3);
+
+// document.writeln("String: " + word + "<br>");
+// document.write("Character at index 3: " + char);
+// _________________________________________________________________________
+
+// 1. Write a program that takes two user inputs for first and 
+// last name using prompt and merge them in a new variable 
+// titled fullName. Greet the user using his full name.
+
+// let firstName = prompt("Enter your first name");
+// let lastName = prompt("Enter your last name");
+
+
+// let fullName = firstName.concat(" ", lastName);
+
+// document.writeln("Hello " + fullName);
+
+// _________________________________________________________________________
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the 
+// word “Hyderabad” and display the result in your browser.
+
+// let word="Hyderabad";
+// let newWord=word.replace("Hyder","Islam");
+
+// document.write("Before: " + word + "<br>");
+// document.write("After: " + newWord);
+// _________________________________________________________________________
+
+// 8. Write a program to replace all occurrences of “and” in the 
+// string with “&” and display the result in your browser.
+
+
+// var message = "Ali and Sami are best friends. They play cricket and football together."; 
+
+// var update=message.replaceAll("and","&");
+
+// document.write("Before: " + message + "<br><br>");
+// document.write("After: " + update);
+// _________________________________________________________________________
+
+// 9. Write a program that converts a string “472” to a number 
+// 472. Display the values & types in your browser.
+
+// let str="472";
+// let num=parseInt(str);
+
+// document.writeln("Value: " + num + "<br>");
+// document.writeln("Type: " + typeof num);
+
+// _________________________________________________________________________
+
+// 10. Write a program that takes user input. Convert and 
+// show the input in capital letters.
+
+// let userInput=prompt("Enter any text");
+// let UppercaseInput=userInput.toUpperCase();
+
+
+// document.writeln("User input: " + userInput + "<br>");
+// document.writeln("In capital letters: " + UppercaseInput);
+// _________________________________________________________________________
+
+// 11. 
+// Write a program that takes user input. Convert and 
+// show the input in title case.
+
+
