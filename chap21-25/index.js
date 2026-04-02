6cdd38c58b1ba64731a42071a6a67564e23222d0
hibafayyan <hibafayyan@gmail.com>
2026-04-02 23:12:09 +0500

    add more content

--- chap21-25/index.js
+++ chap21-25/index.js
@@ -117,3 +117,117 @@
 // show the input in title case.
 
 
+// let input =prompt("Enter your text here!");
+// let words = input.toLowerCase().split(' ');
+
+// for (let i=0 ; i<words.length ; i++){
+// words[i]=words[i][0].toUpperCase()+ words[i].slice(1)
+// }
+
+// let finalResult=words.join(' ');
+// document.writeln(`${finalResult}`);
+
+
+
+
+// let paragraph = "my name is salman"
+
+
+// let arr = paragraph.split(" ")
+// let capitalizeArr = []
+
+
+// console.log(arr)
+
+//  for(let i = 0; i < arr.length; i++){
+
+//   let titleCaseWords = arr[i][0].toUpperCase() + arr[i].slice(1)
+//   capitalizeArr.push(titleCaseWords)
+  
+// }
+// console.log(capitalizeArr);
+
+//  let joinReturn = capitalizeArr.join(" ")
+// paragraph = joinReturn
+
+// console.log(paragraph) 
+
+
+
+// practice////////////////////////////
+
+
+// let para=prompt("Enter your text here");
+// let words=para.toLowerCase().split(' ');
+
+// for (let i=0; i<words.length; i++){
+//   words[i]= words[i][0].toUpperCase()+words[i].slice(1)
+// }
+
+// let finalResult=words.join(' ');
+// document.writeln(`${finalResult}`)
+// ____________________________________________________________________________
+
+// 2. Write a program that converts the variable num to 
+// string. 
+// Remove the dot to display “3536” display in your browser.
+
+
+// let num=35.36
+// let str=num.toString();
+
+// console.log(str,typeof(str));
+
+// let result=str.replace(".","");
+
+// console.log(result);
+
+// ___________________________________________________________________________
+
+// 13. Write a program to take user input and store username 
+// in a variable. If the username contains any special symbol 
+// among [@ . , !], prompt the user to enter a valid username. 
+// For character codes of [@ . 
+// Note: 
+// ASCII code of ! is 33 
+// ASCII code of , is 44 
+// ASCII code of . is 46 
+// ASCII code of @ is 64
+// solution 1 
+
+// let userName = prompt("Enter Username");
+// let isValid = true;
+
+// for (let i=0; i<userName.length ; i++){
+// let charCode = userName.charCodeAt(i);
+
+// if (charCode===33 || charCode === 44 || charCode === 46 || charCode === 64 ){
+// isValid = false;
+// break;
+// }
+// }
+
+// if (isValid){
+//     alert("Username accepted!!")
+// }else {
+//     alert("Please enter a valid username (symbols like @ . , ! are not allowed.");
+// }
+
+// solution 2
+
+let userName = prompt("Enter Username");
+let specialSymbols = ["@", ".", ",", "!"]  // Hamari banned list
+let hasSpecialChar = false;
+
+for (let i=0 ; i< userName.length; i++){
+   if(specialSymbols.includes(userName[i])){
+    hasSpecialChar = true;
+    break;
+   }
+}
+
+if (hasSpecialChar){
+    alert("Please enter a valid username without @, ., , or !")
+}else{
+    alert("Username saved: " + userName);
+}
