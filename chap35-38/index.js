95bd13baf4d38d3f9b1fca6a2486c244422a579d
hibafayyan <hibafayyan@gmail.com>
2026-04-06 04:38:26 +0500

    add assignment

---
+++ chap35-38/index.js
@@ -0,0 +1,8 @@
+// 1. Write a function that displays current date & time in your
+// browser
+function displayDateTime() {
+  let now = new Date();
+  console.log(now);
+}
+//  console.log(now());
+
